1237c179c9b50d8bc716a01bd29aa48006167214
Tori Snowden <tori@Toris-MacBook-Pro.local>
2019-03-25 15:22:48 -0600

    data structures

--- js/app.js
+++ js/app.js
@@ -1,1 +1,52 @@
 console.log('homework');
+
+//1. Data types
+//light switch - For turning a light switch on and off I would use the 
+
+//Boolean, true = on and false = off.
+
+//email - I would use a string. let myEmail = "tori@gmail.com"
+
+//spaceship - I could use and array let spaceship = ["hull", "blasters", tractor beam", "warp drive"];
+
+//list names - For a list of the student's name in class I would use 
+
+//the data type array. (i.e) studentNames = ["Tom", "Will", "James", "Timmy"]
+
+//list names, location - For a lof of student names and location I would use 
+//an object and array. 
+/* Ex. const students = {
+studentNames: ["Tom", "Will", "James", "Timmy"],
+location: "Denver",
+} */
+
+//list names, location, tv - For a lof of student names, location and favorite TV show
+//I would use an object and array.
+/* Ex.  const students = {
+studentNames: ["Tom", "Will", "James", "Timmy"],
+location: "Denver",
+favTvShow: ["Rick and Morty", "Bob's Burgers", "Champions", "Race to Dragon's Edge"]
+} */
+//Or you could use indivdual objects (i.e.) 
+/*Ex.  const student = {
+Names: "Tom",
+location: "Denver",
+favTvShow: "Rick and Morty",
+}*/
+
+//2. Take it Easy
+//1. 
+const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+//2.
+rainbow[4];
+console.log(rainbow[4]);
+//3. 
+const tori = {
+    "favFood": "Mozzerella Sticks",
+    "hobby": "Drawing",
+    "location": "Arvada",
+    "favDataType": "Objects",
+}
+//4. 
+tori["hobby"]
+console.log(tori["hobby"]);
